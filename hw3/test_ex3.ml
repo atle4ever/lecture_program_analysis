b3ad7e74a44d19537de360ed41114409d3d2e06a
SJ Kim <atle4ever@gmail.com>
2010-03-31 15:49:27 +0900

    hw3: test is done

--- hw3/test_ex3.ml
+++ hw3/test_ex3.ml
@@ -22,10 +22,43 @@ end
 
 module N_FIX = Fix(N)
 
+(*************************************)
+(*************************************)
+
+module OrderedInt : Set.OrderedType with type t = int =
+struct
+  type t = int
+  let compare = compare
+end
+
+module IntSet = Set.Make(OrderedInt)
+
+module IntSetCPO : CPO  with type t = IntSet.t =
+struct
+  type t = IntSet.t
+  exception NoLUB
+  let order (x, y) = IntSet.subset x y
+  let lub (x, y) = IntSet.union x y
+  let bottom = IntSet.empty
+end
+
+module F = Fix(IntSetCPO)
+
+let f x = IntSet.union x (IntSet.singleton 3)
+
+let fixedpoint = F.fix f
+
+let u = IntSet.elements fixedpoint
+
 let test_freevariable _ =
   let f = function (n) -> N.make(3) in
     assert_equal (N.make(3)) (N_FIX.fix(f))
 
+let test_freevariable2 _ =
+  assert_equal [3] u
+
 let suite = "Test ex3" >:::
-  ["test_freevariable" >:: test_freevariable]
+  ["test_freevariable" >:: test_freevariable;
+   "test_freevariable" >:: test_freevariable2]
+
 
